498d6a575947b7fbff9cdcf4107b6da47c67d676
Ryan Malm <ryan.malm@gmail.com>
2017-12-30 09:55:11 -0600

    minimap working

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
 
   var t = 0, last = 0, now = 0,
 
-  playerX = WIDTH/2, playerY = HEIGHT/2,
 
   worldWidth = 10000; worldHeight = 10000;
 
+  playerX = worldWidth/2, playerY = worldHeight/2,
+
+  viewX = playerX - WIDTH/2; viewY = playerY - HEIGHT/2;
   blocks = [];
 
 
@@ -21,7 +23,7 @@ load=()=>{
 
 init=()=>{
   //state vars, initial game state
-  let i = 40000;
+  let i = 60000;
   while(--i){
     blocks.push(
       Math.random()*worldWidth|0, //x
@@ -32,12 +34,12 @@ init=()=>{
       3, //type 0:block, 1:circle, 2: filledCircle, 3:dot
     )
   }
-  let j = 50000;
+  let j = 20000;
   while(--j){
     blocks.push(
       Math.random()*worldWidth|0, //x
       Math.random()*worldHeight|0, //y
-      Math.random()*10+5|0, //WIDTH or Radius
+      Math.random()*20+5|0, //WIDTH or Radius
       Math.random()*10+5|0, //HEIGHT
       Math.random()*64|0, //color
       Math.random()*3|0, //type 0:block, 1:circle, 2: filledCircle, 3:dot
@@ -47,59 +49,7 @@ init=()=>{
   loop();
 }
 
-loop=()=>{
-  stats.begin();
-  t++;
-
-  step(t);
-  draw(t);
-
-  render(t);
-
-  stats.end();
-  requestAnimationFrame(loop);
-
-}
-
-step=(dt)=>{
-if(t%5<1){
-  // for(let i = 0; i < blocks.length; i+=6){
-  //     blocks[i] += Math.random()-.5;
-  //     blocks[i+1]+= Math.random()-.5;
-  // }
-}
-
-if(Key.isDown(Key.d) || Key.isDown(Key.RIGHT))       { playerX++; viewX++}
-else if(Key.isDown(Key.a)|| Key.isDown(Key.LEFT)) { playerX--; viewX--;}
-if(Key.isDown(Key.w)|| Key.isDown(Key.UP))      { playerY--; viewY--;}
-else if(Key.isDown(Key.s)|| Key.isDown(Key.DOWN))  { playerY++; viewY++;}
-}
-
-
-draw=(dt)=>{
- clear(30);
- //let i = blocks.length;
- ellipse(20,20,60,25,4);
- fillRect(playerX-viewX, playerY-viewY, 8, 8, 4);
-
-}
-
-window.addEventListener('keyup', function (event) {
-  Key.onKeyup(event);
-}, false);
-window.addEventListener('keydown', function (event) {
-  Key.onKeydown(event);
-}, false);
-window.addEventListener('blur', function (event) {
-  paused = true;
-}, false);
-window.addEventListener('focus', function (event) {
-  paused = false;
-}, false);
-
-load();
-
-drawThings=()=>{
+drawThings=(dt)=>{
 
   for(let i = 0; i < blocks.length; i+=6){
       let x = blocks[i]-viewX,
@@ -108,7 +58,7 @@ drawThings=()=>{
        wr = blocks[i+2],
        h = blocks[i+3],
        type = blocks[i+5],
-       p = 100;
+       p = 60; //overscan check to prevent popping and too much overdraw
 
       if(x > 0-p && x < WIDTH+p && y > 0-p && y < HEIGHT+p){
         switch(type){
@@ -132,5 +82,79 @@ drawThings=()=>{
     }
 
 }
+
+drawMiniMap=(dt)=>{
+  let scalar = 64/10000,
+  x = 320-70,
+  y = 180-70;
+  fillRect(x,y, 64,64, 1);
+  for(let i = 0; i < blocks.length; i+=6){
+      let ex = blocks[i]*scalar,
+       ey = blocks[i+1]*scalar,
+       c = blocks[i+4],
+       wr = blocks[i+2],
+       h = blocks[i+3],
+       type = blocks[i+5],
+       p = 100;
+
+      if(type == 2 && wr > 23){
+          pset(x + ex,y + ey,c)
+      }
+    }
+    pset(x+playerX*scalar, y+playerY*scalar, 4);
+
+}
+loop=()=>{
+  stats.begin();
+  t++;
+
+  step(t);
+  draw(t);
+
+  render(t);
+
+  stats.end();
+  requestAnimationFrame(loop);
+
+}
+
+step=(dt)=>{
+  if(t%5<1){
+    // for(let i = 0; i < blocks.length; i+=6){
+    //     blocks[i] += Math.random()-.5;
+    //     blocks[i+1]+= Math.random()-.5;
+    // }
+  }
+
+  if(Key.isDown(Key.d) || Key.isDown(Key.RIGHT))       { playerX++; viewX++}
+  else if(Key.isDown(Key.a)|| Key.isDown(Key.LEFT)) { playerX--; viewX--;}
+  if(Key.isDown(Key.w)|| Key.isDown(Key.UP))      { playerY--; viewY--;}
+  else if(Key.isDown(Key.s)|| Key.isDown(Key.DOWN))  { playerY++; viewY++;}
+}
+
+draw=(dt)=>{
+ clear(30);
+ //let i = blocks.length;
+ //ellipse(20,20,60,25,4);
+ drawThings();
+ fillRect(playerX-viewX, playerY-viewY, 8, 8, 4);
+ drawMiniMap();
+}
+
+window.addEventListener('keyup', function (event) {
+  Key.onKeyup(event);
+}, false);
+window.addEventListener('keydown', function (event) {
+  Key.onKeydown(event);
+}, false);
+window.addEventListener('blur', function (event) {
+  paused = true;
+}, false);
+window.addEventListener('focus', function (event) {
+  paused = false;
+}, false);
+
+load();
+
 
 })();
